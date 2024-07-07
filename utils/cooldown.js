72c02d2de0293711cc8e46e623083fe488e7d050
lellolidk <104140174+lellolidk@users.noreply.github.com>
2024-07-07 08:17:50 +0200

    Update cooldown.js

--- utils/cooldown.js
+++ utils/cooldown.js
@@ -1,1 +1,16 @@
+const cooldown = new Map();
 
+exports.setCooldown = (id, time) => {
+  cooldown.set(id);
+  setTimeout(() => {
+    this.deleteCooldown(id);
+  }, time);
+};
+
+exports.deleteCooldown = (id) => {
+  cooldown.delete(id);
+};
+
+exports.hasCooldown = (id) => {
+  return cooldown.has(id);
+};
